5c19940edd30276184ff7ac369863395156bd458
Damian Pietryka <damianpietryka2003@gmail.com>
2025-11-05 17:35:43 +0100

    feat: Implement graceful shutdown and reconnection logic

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,35 +1,60 @@
 use futures_util::{SinkExt, StreamExt};
+use std::time::Duration;
+use tokio::time::sleep;
+use tokio_tungstenite::tungstenite::protocol::frame::coding::CloseCode;
 use tokio_tungstenite::tungstenite::Message;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = url::Url::parse("ws://127.0.0.1:8081/")?;
 
-    let (mut ws_stream, _) = tokio_tungstenite::connect_async(url).await?;
+    loop {
+        println!("Attempting to connect to broker...");
+        match tokio_tungstenite::connect_async(url.clone()).await {
+            Ok((mut ws_stream, _)) => {
+                println!("WebSocket connection established to broker.");
 
-    println!("WebSocket connection established to broker.");
+                // Send a message to the broker
+                if let Err(e) = ws_stream.send(Message::Text("Hello from agent!".into())).await {
+                    eprintln!("Error sending message: {}", e);
+                    continue;
+                }
+                println!("Sent 'Hello from agent!' to broker.");
 
-    // Send a message to the broker
-    ws_stream.send(Message::Text("Hello from agent!".into())).await?;
-    println!("Sent 'Hello from agent!' to broker.");
-
-    // Read messages from the broker
-    while let Some(message) = ws_stream.next().await {
-        match message {
-            Ok(msg) => {
-                if msg.is_text() || msg.is_binary() {
-                    println!("Received message from broker: {:?}", msg);
-                } else if msg.is_close() {
-                    println!("Broker disconnected.");
-                    break;
+                // Read messages from the broker
+                while let Some(message) = ws_stream.next().await {
+                    match message {
+                        Ok(msg) => {
+                            if msg.is_text() || msg.is_binary() {
+                                println!("Received message from broker: {:?}", msg);
+                            } else if let Message::Close(Some(close_frame)) = msg {
+                                if close_frame.code == CloseCode::Restart {
+                                    println!("Broker is restarting, will reconnect...");
+                                    break; // Break from the inner loop to reconnect
+                                } else if close_frame.code == CloseCode::Normal {
+                                    println!("Broker is shutting down, exiting.");
+                                    return Ok(()); // Exit the program
+                                } else {
+                                    println!("Broker disconnected: {:?}", close_frame);
+                                    return Ok(()); // Exit the program
+                                }
+                            } else if msg.is_close() {
+                                println!("Broker disconnected without a specific reason.");
+                                break; // Break from the inner loop to reconnect
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("Error receiving message from broker: {}", e);
+                            break; // Break from the inner loop to reconnect
+                        }
+                    }
                 }
             }
             Err(e) => {
-                eprintln!("Error receiving message from broker: {}", e);
-                break;
+                eprintln!("Failed to connect to broker: {}. Retrying in 5 seconds...", e);
             }
         }
-    }
 
-    Ok(())
+        sleep(Duration::from_secs(5)).await;
+    }
 }
--- broker/src/main.rs
+++ broker/src/main.rs
@@ -1,5 +1,8 @@
-use futures_util::{SinkExt, StreamExt};
+use futures_util::{ SinkExt, StreamExt };
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::broadcast;
+use tokio_tungstenite::tungstenite::protocol::frame::coding::CloseCode;
+use tokio_tungstenite::tungstenite::protocol::CloseFrame;
 use tokio_tungstenite::tungstenite::Message;
 
 #[tokio::main]
@@ -8,17 +11,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind(&addr).await?;
     println!("Broker listening on: {}", addr);
 
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(handle_connection(stream));
-    }
+    let (shutdown_tx, _) = broadcast::channel(1);
+
+    let server_future = async move {
+        loop {
+            let shutdown_rx = shutdown_tx.subscribe();
+            tokio::select! {
+                Ok((stream, _)) = listener.accept() => {
+                    tokio::spawn(handle_connection(stream, shutdown_rx));
+                }
+                _ = tokio::signal::ctrl_c() => {
+                    println!("\nCtrl+C received, sending shutdown signal.");
+                    shutdown_tx.send(()).unwrap();
+                    break;
+                }
+            }
+        }
+    };
+
+    server_future.await;
 
     Ok(())
 }
 
-async fn handle_connection(raw_stream: TcpStream) {
+async fn handle_connection(raw_stream: TcpStream, mut shutdown_rx: broadcast::Receiver<()>) {
     println!("Incoming TCP connection from: {}", raw_stream.peer_addr().unwrap());
 
-    let ws_stream = match tokio_tungstenite::accept_async(raw_stream).await {
+    let mut ws_stream = match tokio_tungstenite::accept_async(raw_stream).await {
         Ok(ws) => ws,
         Err(e) => {
             eprintln!("Error during WebSocket handshake: {}", e);
@@ -28,25 +47,40 @@ async fn handle_connection(raw_stream: TcpStream) {
 
     println!("WebSocket connection established.");
 
-    let (mut write, mut read) = ws_stream.split();
-
-    while let Some(message) = read.next().await {
-        match message {
-            Ok(msg) => {
-                if msg.is_text() || msg.is_binary() {
-                    println!("Received a message: {:?}", msg);
-                    // Echo the message back for now
-                    if let Err(e) = write.send(msg).await {
-                        eprintln!("Error sending message: {}", e);
+    loop {
+        tokio::select! {
+            Some(message) = ws_stream.next() => {
+                match message {
+                    Ok(msg) => {
+                        if msg.is_text() || msg.is_binary() {
+                            println!("Received a message: {:?}", msg);
+                            // Echo the message back for now
+                            if let Err(e) = ws_stream.send(msg).await {
+                                eprintln!("Error sending message: {}", e);
+                                break;
+                            }
+                        } else if msg.is_close() {
+                            println!("Client disconnected.");
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Error receiving message: {}", e);
                         break;
                     }
-                } else if msg.is_close() {
-                    println!("Client disconnected.");
-                    break;
                 }
             }
-            Err(e) => {
-                eprintln!("Error receiving message: {}", e);
+            _ = shutdown_rx.recv() => {
+                println!("Shutdown signal received, closing connection.");
+                // To tell the client to reconnect, use CloseCode::Restart
+                // To tell the client not to reconnect, use CloseCode::Normal
+                let close_frame = CloseFrame {
+                    code: CloseCode::Normal,
+                    reason: "Server is shutting down".into(),
+                };
+                if let Err(e) = ws_stream.send(Message::Close(Some(close_frame))).await {
+                    eprintln!("Error sending close frame: {}", e);
+                }
                 break;
             }
         }
